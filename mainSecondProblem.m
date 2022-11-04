d536b59956181cdb39171363474a8d25e627d9cc
Denise Tumiotto <apzns@xd.uni-halle.de>
2022-11-04 11:08:25 +0100

    implemented second problem in matlab - for better visualization

--- mainSecondProblem.m
+++ mainSecondProblem.m
@@ -7,19 +7,36 @@ mu = -2;
 lam = -0.1;
 phi0 = pi/2*(2*rand(2,1)-1);
 theta0 = 2*pi*rand(2,1);
+y01 = sph2cart([phi0(1);theta0(1)]);
+y02 = sph2cart([phi0(2);theta0(2)]);
 
-A = @(y) matrix(y);
+y1 = zeros(3,N+1);
+y2 = zeros(3,N+1);
+y1(:,1) = y01;
+y2(:,1) = y02;
+
+yi1 = zeros(3,N+1);
+yi2 = zeros(3,N+1);
+yi1(:,1) = y01;
+yi2(:,1) = y02;
+
+A = @(y) matrix(y,lam,mu);
 
 lieStep = @(y0,y,dt) expm(dt*A(y))*y0;
 
 implicitLie = @(y0,y,dt) -y+lieStep(y0,y,dt);
 
+for index = 1:N
+    y1(:,index+1) = lieStep(y1(:,1),y1(:,1),h(index));
+    y2(:,index+1) = lieStep(y2(:,1),y2(:,1),h(index));
+    
+    yi1(:,index+1) = fsolve(@(x) implicitLie(yi1(:,1),x,h(index)),yi1(:,index));
+    yi2(:,index+1) = fsolve(@(x) implicitLie(yi2(:,1),x,h(index)),yi2(:,index));
+end
 
+plotSecondProblem(yi1,yi2)
 
-
-
-
-function rslt = matrix(y)
-s = cart2sph(y);
-rslt = skw([sin(s(3))*lam*s(2); -cos(s(3))*lam*s(2); mu*s(3)]);
+function rslt = matrix(y,lam,mu)
+    s = cart2sph(y);
+    rslt = skw([sin(s(3))*lam*s(2); -cos(s(3))*lam*s(2); mu*s(3)]);
 end
---
+++ plotSecondProblem.m
@@ -0,0 +1,70 @@
+function plotSecondProblem(y1,y2)
+
+[~, nTime] = size(y1);
+
+s0(1,:) = cart2sph(y1(1,1), y1(2,1), y1(3,1));
+s0(2,:) = cart2sph(y2(1,1), y2(2,1), y2(3,1));
+
+nSpace = 1000;
+t = linspace(0,1,nSpace);
+geo = zeros(3,nSpace);
+k = 0;
+for i = t
+    k = k + 1;
+    geo(:,k) = ((1-i).*y1(:,1)+i.*y2(:,1))/norm((1-i).*y1(:,1)+i.*y2(:,1));
+end
+
+figure()
+
+% Create sphere surface
+[xS2, yS2, zS2] = sphere(360);
+h = surf(xS2, yS2, zS2, 'FaceAlpha', 0.1); 
+h.EdgeColor = 'none';
+hold on
+% Plot initial values
+plot3(q(1, 1), q(2, 1), q(3, 1), 'o', 'MarkerSize', 5, ...
+'MarkerFaceColor', 'green', ...
+'MarkerEdgeColor', 'none')
+plot3(q(1, 2), q(2, 2), q(3, 2), 'o', 'MarkerSize', 5, ...
+'MarkerFaceColor', 'blue', ...
+'MarkerEdgeColor', 'none')
+% Plot geodesic between initial values and equilibrium point
+plot3(geo(1,:), geo(2,:), geo(3,:), 'LineWidth', 5, 'Color', 'r')
+plot3(1,0,0, 'ok', 'MarkerSize',5, 'MarkerFaceColor', 'k')
+
+% Evaluate and plot exact solution
+qe1 = zeros(3,nSpace);
+qe2 = zeros(3,nSpace);
+k = 0;
+for i = linspace(0.01,15,nSpace)
+    k = k+1;
+    qe1(:,k) = exactSol(s0(1,2:3),i);
+    qe2(:,k) = exactSol(s0(2,2:3),i);
+    plot3(qe1(1,k),qe1(2,k),qe1(3,k), 'o', 'MarkerSize', 3, ...
+    'MarkerFaceColor', 'green', ...
+    'MarkerEdgeColor', 'none')
+    plot3(qe2(1,k),qe2(2,k),qe2(3,k), 'o', 'MarkerSize', 3, ...
+    'MarkerFaceColor', 'blue', ...
+    'MarkerEdgeColor', 'none')
+end
+
+for k = 1:nTime
+    plot3(y1(1,k),y1(2,k),y1(3,k), 'x', 'MarkerSize', 3, ...
+    'MarkerFaceColor', 'green', ...
+    'MarkerEdgeColor', 'none')
+    plot3(y2(1,k),y2(2,k),y2(3,k), 'x', 'MarkerSize', 3, ...
+    'MarkerFaceColor', 'blue', ...
+    'MarkerEdgeColor', 'none')
+end
+
+title(['($\phi_0,\theta_0$)=(',num2str(s0(1,2)),', ',num2str(s0(1,3)),'), ', ...
+       '($\tilde{\phi}_0,\tilde{\theta}_0$)=(',num2str(s0(2,2)),', ',num2str(s0(2,3)),')'], ...
+        'Interpreter', 'latex')
+
+function rslt = exactSol(x0, t)
+    phi = exp(-0.1*t)*x0(1);
+    theta = exp(-2*t)*x0(2);
+    
+    rslt = sph2cart([phi,theta]);
+end
+end

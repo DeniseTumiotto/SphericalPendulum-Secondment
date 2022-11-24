e712a2a15d66211eb6305875f3d5b8eeafd53b20
Denise Tumiotto <denise.tumiotto@gmail.com>
2022-11-24 10:42:35 +0100

    new file for S2 example

--- extras/MATLAB/logNormS2.m
+++ extras/MATLAB/logNormS2.m
@@ -1,12 +1,31 @@
+clearvars
+clc
+
 syms phi theta t
 syms a c
 
-x = cos(phi(t))*cos(theta(t));
-y = cos(phi(t))*sin(theta(t));
+x = cos(phi)*cos(theta);
+y = cos(phi)*sin(theta);
 z = sin(phi);
 
-m = @(xx) (a*cos(phi)^2+c*sin(phi)^2-xx);
+dphi = (a-c)*z*sqrt(x^2+y^2);
+dtheta = (2*x*y)/(x^2+y^2) * (a*x+a*y+c*z-a);
 
-dx = m(a)*cos(phi)*cos(theta);
-dy = m(a)*cos(phi)*sin(theta);
-dz = m(c)*sin(phi);
+
+B = [diff(dphi,phi) diff(dphi,theta);
+     diff(dtheta,phi) diff(dtheta,theta)];
+
+A = [0 dtheta*sin(phi)*cos(theta);
+     -dtheta*tan(phi) -dphi*tan(phi)];
+
+L = A+B;
+
+G = [1 0;
+     0 cos(phi)^2];
+
+J = 0.5*((G*L)/G+transpose(L));
+
+[V,D] = eig(J);
+
+D = subs(D, {phi, theta}, {atan(y/x),asin(z)});
+disp(simplify(diag(D)))
